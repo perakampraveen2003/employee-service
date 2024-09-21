350b9b3656840173f9224d672a324fd77c12160b
Rohith <manipiradhi@gmail.com>
2024-09-21 11:04:34 +0530

    added logger

--- src/main/java/com/miniproject/employee_service/EmployeeServiceApplication.java
+++ src/main/java/com/miniproject/employee_service/EmployeeServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("welcome");
 		SpringApplication.run(EmployeeServiceApplication.class, args);
 	}
 
