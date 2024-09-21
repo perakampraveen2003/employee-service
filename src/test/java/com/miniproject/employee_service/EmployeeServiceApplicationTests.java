d3734940fb742a962f156a1c9616fb716c2e161e
praveen <praveen@gmail.com>
2024-09-21 11:01:07 +0530

    initial commit

---
+++ src/test/java/com/miniproject/employee_service/EmployeeServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.miniproject.employee_service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
